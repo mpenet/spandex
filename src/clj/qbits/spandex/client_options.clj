mpenet/spandex

commit 55255801d76af6d38b0b6a42662b31324628da19

pass builder to config callbacks otherwise they're a bit useless

src/clj/qbits/spandex/client_options.clj
@@ -13,12 +13,12 @@
 (defn request-config-callback [f]
   (reify RestClientBuilder$RequestConfigCallback
     (customizeRequestConfig [this builder]
-      (f))))
+      (f b))))
 
 (defn request-http-client-config-callback [f]
   (reify RestClientBuilder$HttpClientConfigCallback
     (customizeHttpClient [this builder]
-      (f))))
+      (f b))))
 
 (defmulti set-option! (fn [k builder option] k))
 

commit 2bfe847eabf46588372b26cd4556318d7b4f61f8

Fix build (broken since 55255801d76af6d38b0b6a42662b31324628da19)

src/clj/qbits/spandex/client_options.clj
@@ -13,12 +13,12 @@
 (defn request-config-callback [f]
   (reify RestClientBuilder$RequestConfigCallback
     (customizeRequestConfig [this builder]
-      (f b))))
+      (f builder))))
 
 (defn request-http-client-config-callback [f]
   (reify RestClientBuilder$HttpClientConfigCallback
     (customizeHttpClient [this builder]
-      (f b))))
+      (f builder))))
 
 (defmulti set-option! (fn [k builder option] k))
 