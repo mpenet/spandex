9c9c145d5a97c618873b270d542b86f72dd402b8
Reynald Borer <reynald.borer@paper.li>
2017-01-13 08:30:07 +0100

    Bump client to 5.1.2

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/core.async "0.2.395"]
-                 [org.elasticsearch.client/rest "5.1.1"]
-                 [org.elasticsearch.client/sniffer "5.1.1"]
+                 [org.elasticsearch.client/rest "5.1.2"]
+                 [org.elasticsearch.client/sniffer "5.1.2"]
                  [cc.qbits/commons "0.4.6"]
                  [cheshire "5.6.3"]]
   :source-paths ["src/clj"]
