02db2efcf98c59145fe009f2be16ce81e5aca483
Max Penet <m@qbits.cc>
2017-01-23 14:49:32 +0100

    make client and http options open for extension (multimethod)

--- src/clj/qbits/spandex/client_options.clj
+++ src/clj/qbits/spandex/client_options.clj
@@ -32,6 +32,113 @@
     (customizeHttpClient [this builder]
       (f builder))))
 
+;; request opts
+(defmulti set-request-option! (fn [k builder option] k))
+
+(defmethod set-request-option! :authentication?
+  [_ ^RequestConfig$Builder builder authentication?]
+  (.setAuthenticationEnabled builder (boolean authentication?)))
+
+(defmethod set-request-option! :circular-redirect-allowed?
+  [_ ^RequestConfig$Builder builder circular-redirect-allowed?]
+  (.setCircularRedirectsAllowed builder (boolean circular-redirect-allowed?)))
+
+(defmethod set-request-option! :connect-timeout
+  [_ ^RequestConfig$Builder builder connect-timout]
+  (.setConnectTimeout builder (int connect-timout)))
+
+(defmethod set-request-option! :connection-request-timeout
+  [_ ^RequestConfig$Builder builder connection-request-timeout]
+  (.setConnectionRequestTimeout builder (int connection-request-timeout)))
+
+(defmethod set-request-option! :content-compression?
+  [_ ^RequestConfig$Builder builder content-compression?]
+  (.setContentCompressionEnabled builder (boolean content-compression?)))
+
+(defmethod set-request-option! :cookie-spec
+  [_ ^RequestConfig$Builder builder cookie-spec]
+  (.setCookieSpec builder cookie-spec))
+
+(defmethod set-request-option! :decompression?
+  [_ ^RequestConfig$Builder builder decompression?]
+  (.setDecompressionEnabled builder (boolean decompression?)))
+
+(defmethod set-request-option! :expect-continue?
+  [_ ^RequestConfig$Builder builder expect-continue?]
+  (.setExpectContinueEnabled builder (boolean expect-continue?)))
+
+(defmethod set-request-option! :local-address
+  [_ ^RequestConfig$Builder builder local-address]
+  (.setLocalAddress builder local-address))
+
+(defmethod set-request-option! :max-redirects
+  [_ ^RequestConfig$Builder builder max-redirects]
+  (.setMaxRedirects builder (int max-redirects)))
+
+(defmethod set-request-option! :proxy
+  [_ ^RequestConfig$Builder builder proxy]
+  (.setProxy builder proxy))
+
+(defmethod set-request-option! :redirects?
+  [_ ^RequestConfig$Builder builder redirects?]
+  (.setRedirectsEnabled builder (boolean redirects?)))
+
+(defmethod set-request-option! :relative-redirects-allowed?
+  [_ ^RequestConfig$Builder builder relative-redirects-allowed?]
+  (.setRelativeRedirectsAllowed builder (boolean relative-redirects-allowed?)))
+
+(defmethod set-request-option! :socket-timeout
+  [_ ^RequestConfig$Builder builder socket-timeout]
+  (.setSocketTimeout builder (int socket-timeout)))
+
+(defmethod set-request-option! :target-preferred-auth-schemes
+  [_ ^RequestConfig$Builder builder target-preferred-auth-schemes]
+  (.setTargetPreferredAuthSchemes builder target-preferred-auth-schemes))
+
+
+;; http-client options
+(defmulti set-http-client-option! (fn [k builder option] k))
+
+(defmethod set-http-client-option! :max-conn-per-route
+  [_ ^HttpAsyncClientBuilder builder max-conn-per-route]
+  (.setMaxConnPerRoute builder (int max-conn-per-route)))
+
+(defmethod set-http-client-option! :max-conn-total
+  [_ ^HttpAsyncClientBuilder builder max-conn-total]
+  (.setMaxConnTotal builder (int max-conn-total)))
+
+(defmethod set-http-client-option! :proxy
+  [_ ^HttpAsyncClientBuilder builder proxy]
+  (.setProxy builder proxy))
+
+(defmethod set-http-client-option! :ssl-context
+  [_ ^HttpAsyncClientBuilder builder ssl-context]
+  (.setSSLContext builder ssl-context))
+
+(defmethod set-http-client-option! :user-agent
+  [_ ^HttpAsyncClientBuilder builder user-agent]
+  (.setUserAgent builder user-agent))
+
+(defmethod set-http-client-option! :auth-caching?
+  [_ ^HttpAsyncClientBuilder builder auth-caching?]
+  (cond-> builder
+    (not auth-caching?) (.disableAuthCaching)))
+
+(defmethod set-http-client-option! :cookie-management?
+  [_ ^HttpAsyncClientBuilder builder cookie-management?]
+  (cond-> builder
+    (not cookie-management?) (.disableCookieManagement)))
+
+(defmethod set-http-client-option! :basic-auth
+  [_ ^HttpAsyncClientBuilder builder {:keys [user password]}]
+  (-> builder
+      (.setDefaultCredentialsProvider
+       (doto (BasicCredentialsProvider.)
+         (.setCredentials AuthScope/ANY
+                          (UsernamePasswordCredentials. user
+                                                        password))))))
+
+;; top level options
 (defmulti ^:no-doc set-option! (fn [k builder option] k))
 
 (defmethod set-option! :max-retry-timeout
@@ -45,106 +152,27 @@
                                                      (BasicHeader. (name k) v))
                                                    headers)))))
 
-(defmethod set-option! :http-client-config-callback
-  [_ ^RestClientBuilder builder f]
-  (-> builder (.setHttpClientConfigCallback (http-client-config-callback f))))
-
-(defmethod set-option! :request-config-callback
-  [_ ^RestClientBuilder builder f]
-  (-> builder (.setRequestConfigCallback (request-config-callback f))))
-
 (defmethod set-option! :request
-  [_ ^RestClientBuilder builder
-   {:as opts
-    :keys [connection-request-timeout
-           connect-timout
-           cookie-spec
-           decompression?
-           content-compression?
-           authentication?
-           circular-redirect?
-           expect-continue?
-           local-address
-           max-redirects
-           proxy
-           redirects?
-           relative-redirects?
-           socket-timeout
-           preferred-auth-schemes]}]
+  [_ ^RestClientBuilder builder options]
+  ;; this dispatches on set-request-option!, see the following link for more details
   ;; https://hc.apache.org/httpcomponents-client-ga/httpclient/apidocs/org/apache/http/client/config/RequestConfig.Builder.html
-  (-> builder
-      (.setRequestConfigCallback
-       (request-config-callback
-        #(cond-> ^RequestConfig$Builder %
-           authentication?
-           (.setAuthenticationEnabled (boolean authentication?))
-           circular-redirect?
-           (.setCircularRedirectsAllowed (boolean circular-redirect?))
-           connect-timout
-           (.setConnectTimeout (int connect-timout))
-           connection-request-timeout
-           (.setConnectionRequestTimeout (int connection-request-timeout))
-           content-compression?
-           (.setContentCompressionEnabled (boolean content-compression?))
-           cookie-spec
-           (.setCookieSpec cookie-spec)
-           decompression?
-           (.setDecompressionEnabled (boolean decompression?))
-           expect-continue?
-           (.setExpectContinueEnabled (boolean expect-continue?))
-           local-address
-           (.setLocalAddress local-address)
-           max-redirects
-           (.setMaxRedirects max-redirects)
-           proxy
-           (.setProxy proxy)
-           redirects?
-           (.setRedirectsEnabled redirects?)
-           relative-redirects?
-           (.setRelativeRedirectsAllowed relative-redirects?)
-           socket-timeout
-           (.setSocketTimeout (int socket-timeout))
-           preferred-auth-schemes
-           (.setTargetPreferredAuthSchemes preferred-auth-schemes))))))
+  (.setRequestConfigCallback builder
+                             (request-config-callback
+                              #(reduce (fn [builder [k option]]
+                                         (set-request-option! k builder option))
+                                       %
+                                       options))))
 
 (defmethod set-option! :http-client
-  [_ ^RestClientBuilder builder
-   {:as opts
-    :keys [max-conn-per-route
-           max-conn-total
-           proxy
-           ssl-context
-           user-agent
-           ;; automatic-retries?
-           auth-caching?
-           cookie-management?
-           basic-auth]}]
+  [_ ^RestClientBuilder builder options]
+  ;; this dispatches on set-http-client-option!, see the following link for more details
   ;; https://hc.apache.org/httpcomponents-asyncclient-dev/httpasyncclient/apidocs/org/apache/http/impl/nio/client/HttpAsyncClientBuilder.html
-  (-> builder
-      (.setRequestConfigCallback
-       (http-client-config-callback
-        #(cond-> ^HttpAsyncClientBuilder %
-           max-conn-per-route
-           (.setMaxConnPerRoute (int max-conn-per-route))
-           max-conn-total
-           (.setMaxConnTotal (int max-conn-total))
-           proxy
-           (.setProxy proxy)
-           ssl-context
-           (.setSSLContext ssl-context)
-           user-agent
-           (.setUserAgent user-agent)
-           (not auth-caching?)
-           (.disableAuthCaching)
-           (not cookie-management?)
-           (.disableCookieManagement)
-           basic-auth
-           (.setDefaultCredentialsProvider
-            (doto (BasicCredentialsProvider.)
-              (.setCredentials AuthScope/ANY
-                               (UsernamePasswordCredentials.
-                                (:user basic-auth)
-                                (:password basic-auth))))))))))
+  (.setHttpClientConfigCallback builder
+                                (http-client-config-callback
+                                 #(reduce (fn [builder [k option]]
+                                            (set-http-client-option! k builder option))
+                                          %
+                                          options))))
 
 (defmethod set-option! :sniff-on-failure
   [_ ^RestClientBuilder builder sniffer]
