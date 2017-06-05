aaac0e50413e906d0008e318c83ee9305f0da751
Max Penet <m@qbits.cc>
2017-06-05 09:20:09 +0200

    fix bulk chan content-type (now required)

--- src/clj/qbits/spandex.clj
+++ src/clj/qbits/spandex.clj
@@ -442,10 +442,10 @@
                 :or {flush-interval 5000
                      flush-threshold 300
                      max-concurrent-requests 3}}]
-       (let [request-map (merge {:url "/_bulk"
-                                 :method :put
-                                 :headers {"content-type" "text/plain"}}
-                                request-map)
+       (let [request-map (conj {:url "/_bulk"
+                                :method :put
+                                :headers {"Content-Type" "application/x-ndjson"}}
+                               request-map)
              input-ch (or input-ch (async/chan))
              output-ch (or output-ch (async/chan))
              request-ch (async/chan max-concurrent-requests)]
