e910c2a382f6c6d48a7d7b397dd0cbfcd2c5fe26
Max Penet <m@qbits.cc>
2018-02-28 12:02:08 +0100

    bump commons

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def es-client-version "6.2.1")
-(defproject cc.qbits/spandex "0.6.0"
+(defproject cc.qbits/spandex "0.6.1"
   :description "Clojure Wrapper of the new/official ElasticSearch REST client"
   :url "https://github.com/mpenet/spandex"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
                  [org.elasticsearch.client/elasticsearch-rest-client ~es-client-version]
                  [org.elasticsearch.client/elasticsearch-rest-client-sniffer ~es-client-version
                    :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [cc.qbits/commons "0.4.6"]
+                 [cc.qbits/commons "0.5.1"]
                  [cheshire "5.8.0"]
                  [ring/ring-codec "1.1.0"]]
   :source-paths ["src/clj"]
