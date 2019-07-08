68c13b3069771c83e8eb8a31a518bcf6bed19bb5
Max Penet <m@qbits.cc>
2019-07-08 09:11:25 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def es-client-version "7.2.0")
-(defproject cc.qbits/spandex "0.7.0"
+(defproject cc.qbits/spandex "0.7.1"
   :description "Clojure Wrapper of the new/official ElasticSearch REST client"
   :url "https://github.com/mpenet/spandex"
   :license {:name "Eclipse Public License"
