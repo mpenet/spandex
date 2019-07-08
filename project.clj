30305150d5157af66e958beb38b774a8b8ce2e52
Max Penet <m@qbits.cc>
2019-07-08 09:10:52 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(def es-client-version "7.1.0")
+(def es-client-version "7.2.0")
 (defproject cc.qbits/spandex "0.7.0"
   :description "Clojure Wrapper of the new/official ElasticSearch REST client"
   :url "https://github.com/mpenet/spandex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1-RC1"]
-                 [org.clojure/core.async "0.4.490"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.4.500"]
                  [org.elasticsearch.client/elasticsearch-rest-client ~es-client-version]
                  [org.elasticsearch.client/elasticsearch-rest-client-sniffer ~es-client-version
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [cc.qbits/commons "0.5.1"]
+                 [cc.qbits/commons "0.5.2"]
                  [cheshire "5.8.1"]
                  [ring/ring-codec "1.1.1"]]
   :source-paths ["src/clj"]
