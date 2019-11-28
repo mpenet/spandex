870edd4d0123a2aa454314ff1cdd3442f233c94e
Max Penet <max.penet@exoscale.ch>
2019-11-28 12:14:37 +0100

    Version 0.7.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def es-client-version "7.4.2")
-(defproject cc.qbits/spandex "0.7.3"
+(defproject cc.qbits/spandex "0.7.4-SNAPSHOT"
   :description "Clojure Wrapper of the new/official ElasticSearch REST client"
   :url "https://github.com/mpenet/spandex"
   :license {:name "Eclipse Public License"
